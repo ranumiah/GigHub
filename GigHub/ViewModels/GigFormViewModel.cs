d02a12d12005fb6b7c4f6152268ca45494ae0161
Ranu <O@O.net>
2016-12-07 18:17:13 +0000

    add custom ValidationAttribute for Date & Time

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -11,9 +11,11 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
 
         [Required]
+        [FutureDate]
         public string Date { get; set; }
 
         [Required]
+        [ValidTime]
         public string Time { get; set; }
 
         [Required]

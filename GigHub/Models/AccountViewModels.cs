9e9ebb9517f1c4aef851c69f6b86003f217499d1
Ranu <O@O.net>
2016-12-07 15:20:36 +0000

    add UserName as primary form of login to the system instead of Email

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace GigHub.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,10 @@ namespace GigHub.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
@@ -83,6 +86,10 @@ namespace GigHub.Models
 
     public class ResetPasswordViewModel
     {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]

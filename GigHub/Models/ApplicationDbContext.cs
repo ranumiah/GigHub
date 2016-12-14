0808f7192bd71d8e2bb23169454b1fa5cedcf006
Ranu <O@O.net>
2016-12-14 16:10:56 +0000

    Add new Attendance table with EF Fluent Migration

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using System.Data.Entity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GigHub.Models
 {
@@ -8,6 +8,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -17,6 +18,16 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }

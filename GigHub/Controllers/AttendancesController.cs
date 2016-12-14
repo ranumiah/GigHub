25c2ef93056215ae5e8c1d7777346ecf909e15f4
Ranu <O@O.net>
2016-12-14 16:56:44 +0000

    Wired WebApi Attend Post request to button control

--- GigHub/Controllers/AttendancesController.cs
+++ GigHub/Controllers/AttendancesController.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace GigHub.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+                return BadRequest("The attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }

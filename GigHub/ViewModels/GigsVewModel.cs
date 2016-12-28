815f40ce512da9bb11f60c9fb6425090ee326609
Ranu <O@O.net>
2016-12-28 16:47:02 +0000

    Implement View Gigs I'm Attending

---
+++ GigHub/ViewModels/GigsVewModel.cs
@@ -0,0 +1,12 @@
+using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigsVewModel
+    {
+        public IEnumerable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+        public string Heading { get; set; }
+    }
+}

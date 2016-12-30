24e786bbf2a012b4e8599edd8c102b67f1cbefa6
Ranu <O@O.net>
2016-12-30 09:37:27 +0000

    Refactor Magic String into Lambda Expressions

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,7 +1,10 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
@@ -26,7 +29,28 @@ namespace GigHub.ViewModels
 
         public string Heading { get; set; }
 
-        public string Action => (Id != 0) ? "Update" : "Create";
+        public string Action
+        {
+            get
+            {
+                // Func is a Delegate, which allows us to call the anonymous method
+                Expression<Func<GigsController, ActionResult>> update =
+                    (c => c.Update(this)); // This is an anonymous method.
+
+                // Expression ==> prevents the Func to be called instead it just represent the expression
+                Expression<Func<GigsController, ActionResult>> create =
+                    (c => c.Create(this));
+
+                var action = Id != 0 ? update : create;
+
+                // Any change to the actuall class methods will be pick up here.
+                // Therefore avoiding the pitfull of magic string.
+                var methodCallExpression = action.Body as MethodCallExpression;
+
+                // Check for null; If null then return null ELSE return string
+                return methodCallExpression?.Method.Name;
+            }
+        }
 
         public DateTime GetDateTime()
         {
